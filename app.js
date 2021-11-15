8034a18d62870aded9e416eea8d1aee8c428b76d
CamilaPecho <camila_pfccta@hotmail.com.ar>
2021-11-15 16:37:55 -0300

    Inu-Agregando mas cosas al home

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
